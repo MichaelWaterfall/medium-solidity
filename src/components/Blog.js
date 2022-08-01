5153159378571fca8195db0ace7d7a75bbf66e22
Michael Waterfall <michaeljames.waterfall@gmail.com>
2022-08-01 13:24:02 +0100

    config

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./Blog.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Url } from "./config/constants";
+import { Url } from "./constants";
 
 
 const Blog = () => {
